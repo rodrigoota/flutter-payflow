fcb68ae8932a81175f94e5148c2dca50fe8488e3
rodrigoota <rodrigotety@hotmail.com>
2021-06-27 08:49:25 -0300

    creating new project

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/modules/splash/splash_page.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+
+void main() {
+  runApp(AppWidget());
+}
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //debugShowCheckedModeBanner: false,   //<-- remover o banner de debug
+      title: 'Pay Flow',
+      theme: ThemeData(
+        primaryColor: AppColors.primary,
+      ),
+      home: const SplashPage(),
+    );
+  }
+}
